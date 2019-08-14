ab9012244ac6c106e7f775e579a7e73c48c286c7
Cong <rampadc@users.noreply.github.com>
2019-08-14 15:51:44 +1000

    Added Gulp tasks to rebuild and respin up Node-RED

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,45 @@
 const del = require('del');
 const ts = require('gulp-typescript');
-const { src, dest, series, parallel } = require('gulp');
+const { src, dest, series, parallel, watch, task } = require('gulp');
+
+const shell = require('gulp-shell');
 const tsProject = ts.createProject('tsconfig.json');
 
-function cleanTask() {
+task('clean', function() {
   return del('dist/**');
-}
+});
 
-function createTs() {
+
+task('compile', function() {
   return tsProject.src().pipe(tsProject()).js.pipe(dest('dist'));
-}
+});
 
-function copyHtml() {
+task('copy-html', function() {
   return src(['src/*.html']).pipe(dest('dist'));
-}
+});
 
-function copyIcons() {
+task('copy-icons', function() {
   return src(['src/icons/**']).pipe(dest('dist/icons'));
-}
+});
 
-exports.clean = cleanTask;
-exports.default = series(cleanTask, parallel(copyHtml, copyIcons, createTs));
+task('build', function(done) {
+  return parallel('copy-html', 'copy-icons', 'compile')(done);
+});
+
+task('node-red-restart', function(done) {
+  return shell.task('docker-compose down && docker-compose up -d')(done);
+});
+
+task('node-red-down', function(done) {
+  return shell.task('docker-compose down')(done);
+});
+
+task('default', function(done) {
+  return series('clean', 'watch')(done);
+});
+
+task('watch', function() {
+  watch('src/**/*', function(done) {
+    return series('clean', 'build', 'node-red-restart')(done);
+  });
+});
