c309883fcadcfe8aa0ea1fe7af6ab2e64544c7b8
Cong <rampadc@users.noreply.github.com>
2019-08-20 22:05:13 +1000

    Added browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ const { src, dest, series, parallel, watch, task } = require('gulp');
 
 const shell = require('gulp-shell');
 const tsProject = ts.createProject('tsconfig.json');
+const browserSync = require('browser-sync').create();
 
 task('clean', function() {
   return del('dist/**');
 });
-
 
 task('compile', function() {
   return tsProject.src().pipe(tsProject()).js.pipe(dest('dist'));
@@ -34,12 +34,28 @@ task('node-red-down', function(done) {
   return shell.task('docker-compose down')(done);
 });
 
+task('browser_sync', function(done) {
+  browserSync.init({
+    proxy: "localhost:1880",
+    ws: true
+  });
+  done();
+});
+
+task('reload', function(done) {
+  // Delay is used to account for any delays from Docker container re-binding to port
+  setTimeout(() => {
+    browserSync.reload();
+  }, 2000);
+  done();
+});
+
 task('default', function(done) {
-  return series('clean', 'watch')(done);
+  return parallel('browser_sync', 'watch')(done);
 });
 
 task('watch', function() {
   watch('src/**/*', function(done) {
-    return series('clean', 'build', 'node-red-restart')(done);
+    return series('clean', 'build', 'node-red-restart', 'reload')(done);
   });
 });
